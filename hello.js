6768030bcbb94bbeddd86fc8edc6f51841835287
sozain <127615313+sozain@users.noreply.github.com>
2024-08-28 20:26:10 +0500

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+ const customerId = 12345;
+let customerName = "sozain";
+var customerAdress = "chowk azam";
+var customerEmail= "sozain@gmail.com";
+ 
+//customerEmail="ali";
+//customerId = 345678;
+//customerName= "ali";
+console.table([customerId,customerName,customerAdress,customerEmail]);
+
+
